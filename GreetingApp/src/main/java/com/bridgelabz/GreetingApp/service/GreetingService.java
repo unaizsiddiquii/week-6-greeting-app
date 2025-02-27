68cd353c295ece5f3fb0e11a940cada05458cfa0
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-02-27 19:59:35 +0530

    [unaiz siddiqui] uc3 - display message with input

--- GreetingApp/src/main/java/com/bridgelabz/GreetingApp/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/GreetingApp/service/GreetingService.java
@@ -2,9 +2,21 @@ package com.bridgelabz.GreetingApp.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class GreetingService {
+
     public String getGreetMessage() {
         return "Hello World";
+    }
+
+    public String getGreetMessage(String firstName, String lastName) {
+        String greetMessage = "Hello ";
+        if (firstName != null && lastName != null) {
+            return greetMessage + firstName + " " + lastName;
+        } else
+            return greetMessage + Objects.requireNonNullElseGet(firstName, () -> Objects.requireNonNullElse(lastName, "World"));
+
     }
 }
